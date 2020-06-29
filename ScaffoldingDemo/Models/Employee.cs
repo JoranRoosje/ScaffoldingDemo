5ea4a76f0fee83105cbb56b5cc30ec0473fd4874
JoranRoosje <joranrooseleer@gmail.com>
2020-06-29 09:21:36 +0200

    Add project files.

---
+++ ScaffoldingDemo/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScaffoldingDemo.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        
+        [MaxLength(50)]
+        public string Firstname { get; set; }
+
+        [MaxLength(50)]
+        public string Lastname { get; set; }
+
+        [ForeignKey("Department")]
+        public int DepartmentId { get; set; }
+
+        public Department Department { get; set; }
+    }
+}
